c91b63f078e665106de192cc80c8f845e6ce353d
Kay Strobach <github@kay-strobach.de>
2013-03-03 16:39:57 +0100

    [TASK] Add fluidpages and fluidcontent ts files

---
+++ Resources/Private/DummyFiles/Resources/Private/TypoScript/Library/extension.fluidcontent.ts
@@ -0,0 +1,9 @@
+// this feature requires ext:fluidcontent
+// place your fce templates in EXT:{###extKey###}/Resources/Private/Elements
+
+plugin.tx_fed.fce.{###extKey###} {
+	label = Additional Elements by {###extKey###}
+	templateRootPath = EXT:{###extKey###}/Resources/Private/Elements/
+	partialRootPath  = EXT:flux/Resources/Private/Partials/
+	layoutRootPath   = EXT:fluidcontent/Resources/Private/Layouts/
+}
---
+++ Resources/Private/DummyFiles/Resources/Private/TypoScript/Library/extension.fluidpages.ts
@@ -0,0 +1,8 @@
+// this feature requires ext:fluidpages
+// place your pages templates in EXT:{###extKey###}/Resources/Private/Templates/Page
+
+plugin.tx_fed.page.fluidpages {
+	templateRootPath = EXT:{###extKey###}/Resources/Private/Templates/
+	partialRootPath  = EXT:{###extKey###}/Resources/Private/Partials/
+	layoutRootPath   = EXT:{###extKey###}/Resources/Private/Layouts/
+}
