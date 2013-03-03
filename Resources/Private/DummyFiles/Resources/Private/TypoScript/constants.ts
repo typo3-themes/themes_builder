4bb4c5d6de43be74f2e86bd05308c9d58971f1c9
Kay Strobach <github@kay-strobach.de>
2013-03-03 15:38:29 +0100

    Init

---
+++ Resources/Private/DummyFiles/Resources/Private/TypoScript/constants.ts
@@ -0,0 +1,3 @@
+# File which just includes other files
+
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:{###extKey###}/Resources/Private/TypoScript/Constants/Basic.ts">
---
+++ Resources/Private/DummyFiles/Resources/Private/TypoScript/setup.ts
@@ -0,0 +1,4 @@
+# File which just includes other files
+
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:{###extKey###}/Resources/Private/TypoScript/Setup/Config.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:{###extKey###}/Resources/Private/TypoScript/Setup/Page.ts">
