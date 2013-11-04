ba3d3dbc070599d748249928f3c7c6b37d713781
Ralf-René Schröder <ralf.rene@image-format.eu>
2013-11-04 21:56:50 +0100

    Update ext_emconf.php
    for using from TYPO3 6.2.x only

--- Resources/Private/DummyFiles/ext_emconf.php
+++ Resources/Private/DummyFiles/ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => '{###description###}',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.0.0',
+	'version' => '0.0.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.5.0-6.0.99',
+			'php' => '5.3.0-0.0.0',
+			'typo3' => '6.1.99-6.2.99',
 		),
 		'conflicts' => array(
 		),
